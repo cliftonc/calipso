2e1bdc0d9a9f0a0b75a023bcefe4b580c6b76338
Jonathan Zacsh <jzacsh@gmail.com>
2011-06-09 22:35:04 -0400

    exiting with failure, to indicate something is wrong. also avoiding unneeded bash interpolation of strings.

--- bin/install.sh
+++ bin/install.sh
@@ -5,29 +5,34 @@
 node=$(node --version)
 npm=$(npm --version)
 
+E_ERROR=1
+
 echo "Using Node version: $node"
 echo "Using NPM version: $npm"
 
 # Check that we have ok permissions
 if [[ -w . ]] ; then
-    echo "You have write permission to the current folder."
+    echo 'You have write permission to the current folder.'
 else
-    echo "You don't have the required read/write access to the Calipso folder! You need to execute: sudo chmod -R u+rw ."
-    exit
+    echo "You don't have the required read/write access to the Calipso folder! You need to execute: sudo chmod -R u+rw ." >&2
+    exit $E_ERROR
 fi
 
 # Check that we are in the root Folder
 if [[ -r ./app.js ]] ; then
-  echo "The current folder appears to be Calipso (or something like it!)"
+  echo 'The current folder appears to be Calipso (or something like it!)'
 else
-  echo "You need to run this command from the Calipso base folder (not bin)."
-  exit
+  echo 'You need to run this command from the Calipso base folder (not bin).' >&2
+  exit $E_ERROR
 fi
 
 # Checking that you have expat
+echo 'Checking for libexpat ...'
 if [[ $(locate libexpat.so > /dev/null) ]]; then
+  echo 'Libexpat OK'
 else
-  echo "Libexpat OK"
+  echo "You don't seem to have libexpat-dev installed!  Calipso relies on this library for the node-expat module used to parse XML.  Please install via apt-get (or from source)." >&2
+  exit $E_ERROR
 fi
 
 # Lets try npm install
@@ -40,20 +45,21 @@ if [[ -r ./node_modules/express ]] ; then
     if [[ -r ./node_modules/node-expat ]] ; then
       echo "It appears that NPM has installed the key modules ..."
     else
-      echo "NPM failed to install node-expat"
-      exit
+      echo 'NPM failed to install node-expat' >&2
+      exit $E_ERROR
     fi
   else
-    echo "NPM failed to install mongodb"
-    exit
+    echo 'NPM failed to install mongodb' >&2
+    exit $E_ERROR
   fi
 else
-  echo "NPM failed to install express"
-  exit
+  echo 'NPM failed to install express' >&2
+  exit $E_ERROR
 fi
 
 # Lets try our sanity test
+echo 'Running sanity tests ...'
 sanityTest=$(make)
 
 # Check to see if we got all 100%
-echo "If you can see 100% success on the sanity tests then you are probably good to go!"
+echo 'If you can see 100% success on the sanity tests then you are probably good to go!'
