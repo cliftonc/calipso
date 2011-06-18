cliftonc/calipso

commit eaf6dbf5cbe13ac3495c81412c2218cfdf4cfc8f

Added skeleton for content versions

modules/core/contentTypes/contentTypes.module.js
@@ -15,7 +15,9 @@ install: install,
     author: 'cliftonc',
     version: '0.1.1',
     home:'http://github.com/cliftonc/calipso'
-  }};
+  },
+  depends:["content"]
+};
 
 /**
  * Router
@@ -40,13 +42,6 @@ function route(req,res,module,app,next) {
  */
 function init(module,app,next) {
 
-  if(!calipso.modules.content.initialised) {
-    process.nextTick(function() { init(module,app,next); });
-    return;
-  }
-
-  module.initialised = false;
-
   calipso.lib.step(
       function defineRoutes() {
 
@@ -62,9 +57,6 @@ function init(module,app,next) {
 
       },
       function done() {
-
-        // Statics
-        app.use(calipso.lib.express.static(__dirname + '/static'));
 
         // Schemea
         var ContentType = new calipso.lib.mongoose.Schema({

modules/core/content/content.module.js
@@ -15,7 +15,9 @@ exports = module.exports = {
     author: 'cliftonc',
     version: '0.1.1',
     home:'http://github.com/cliftonc/calipso'
-  }};
+  },
+  contentForm:contentForm
+};
 
 /**
  * Standard module route function.
@@ -153,7 +155,7 @@ function init(module,app,next) {
 /**
  * Local default for the content create / edit form
  */
-var contentForm = function() {
+function contentForm() {
       return {id:'content-form',title:'Create Content ...',type:'form',method:'POST',action:'/content',tabs:true,
           sections:[{
             id:'form-section-content',
@@ -290,7 +292,7 @@ function titleAlias(title) {
 function getForm(req,action,title,contentType,next) {
 
   // Create the form
-  var form = contentForm();
+  var form = exports.contentForm(); // Use exports as other modules may alter the form function
   form.action = action;
   form.title = title;
 

modules/core/contentVersions/contentVersions.module.js
@@ -1,0 +1,120 @@
+
+/**
+ * Module that allows management of content types
+ * Base content type sub-module [Depends on Content]
+ */
+
+var calipso = require("lib/calipso"), Query = require("mongoose").Query;
+
+exports = module.exports = {
+  init: init,
+  route: route,  
+  about: {
+    description: 'Provides versioning hooks and forms to enable storage and retrieval of different versions of content.',
+    author: 'cliftonc',
+    version: '0.2.0',
+    home:'http://github.com/cliftonc/calipso'
+  },
+  depends:["content"]
+};
+
+/**
+ * Router
+ */
+function route(req,res,module,app,next) {
+      
+      /**
+       * Routing and Route Handler
+       */
+      module.router.route(req,res,next);
+
+}
+
+/**
+ *Init
+ */
+function init(module,app,next) {
+
+  calipso.lib.step(
+      function defineRoutes() {
+
+        // Crud operations
+        module.router.addRoute('GET /content/show/:id/versions',listVersions,{admin:true,template:'show',block:'content.version'},this.parallel());
+        module.router.addRoute('GET /content/show/:id/version/:version',showVersion,{admin:true,template:'show',block:'content.version'},this.parallel());
+        module.router.addRoute('GET /content/show/:id/diff/:a',diffVersion,{admin:true,template:'show',block:'content.version'},this.parallel());
+        module.router.addRoute('GET /content/show/:id/diff/:a/:b',diffVersion,{admin:true,template:'show',block:'content.version'},this.parallel());
+
+      },
+      function done() {
+        
+        // Schemea
+        var ContentVersion = new calipso.lib.mongoose.Schema({
+          contentId:{type: String},
+          version: {type: Number},
+          author: {type: String},
+          comment: {type: String},
+          created: {type: Date, default: Date.now},
+          updated: {type: Date, default: Date.now}          
+        });
+
+        calipso.lib.mongoose.model('ContentVersion', ContentVersion);
+
+        var Content = calipso.lib.mongoose.model('Content');
+        
+        // Content pre-save hook
+        Content.schema.post('save',function() {
+
+        });
+
+        // Apply the alteration to the content form
+        calipso.modules.content.fn.originalContentForm = calipso.modules.content.fn.contentForm;
+        calipso.modules.content.fn.contentForm = function() {            
+          var form = calipso.modules.content.fn.originalContentForm();
+          form.sections.push(contentVersionFormSection);          
+          return form;
+        }
+       
+        next();
+
+      }
+  );
+}
+
+/**
+ * Section to add to content form
+ */
+var contentVersionFormSection = {
+  id:'form-section-content-version',
+  label:'Versioning',
+  fields:[
+          {label:'Version?',name:'content[version]',type:'select',options:["No","Yes"],description:'This change creates a new version of the content.'},
+          {label:'Comment',name:'content[comment]',type:'textarea',description:'Describe the reason for this version.'},          
+         ]
+}
+
+/**
+ * Show version
+ */
+function showVersion(req,res,template,block,next) {
+
+    res.send(req.moduleParams.version);
+
+}
+
+/**
+ * Show diff between versions
+ */
+function diffVersion(req,res,template,block,next) {
+
+    res.send(req.moduleParams.a + " " + req.moduleParams.b);
+
+}
+
+/**
+ * SHow list of versions
+ */
+function listVersions(req,res,template,block,next) {
+
+    res.send(req.moduleParams.id);
+
+}

modules/core/contentVersions/templates/list.html
@@ -1,0 +1,25 @@
+<div class="template-module">
+    <h2><%- t('Content Types') %></h2>
+    <table>
+    <tr>
+        <th><%- t('Type') %></th><th><%- t('Description') %></th><th><%- t('Layout') %></th><th><%- t('Public') %></th>
+    </tr>
+
+    <%
+        items.forEach(function(item) {
+         %>
+	         <tr>
+	        <td><a href='/content/type/show/<%- item._id %>'><%- item.contentType %></a></td>
+	        <td> <%- item.description %></td>
+	        <td> <%- item.layout %></td>
+	        <td> <%- item.ispublic ? "Yes" : "No" %></td>
+	        </tr>
+	        <%
+	    });
+
+        if(!items) {
+            %><td colspan='4'><%- t('No content types defined.') %></td><%
+        }
+    %>
+    </table>
+</div>

modules/core/contentVersions/templates/show.html
@@ -1,0 +1,8 @@
+ <div class='content-item'>
+    <h2><a href='/<%= item.alias %>.html'><%- item.title %></a></h2>
+    <p class='by'>By <a href='/user/profile/<%- item.author %>'><%- item.author %></a></small>
+    <p class='status'><%- item.status %></p>
+    <p class='created'><%- t('Published') + ': ' + prettyDate(item.published) %> / <%- t('Last Updated') + ': ' + prettyDate(item.updated) %></p>
+    <p class='tags'><%- item.tags %></p>
+    <p class='content'><%- item.content %></p>
+</div>

commit 564cfa11087fc708a4c9d37154993c7523069885

Repaired issue re. wrong dependency between content and contentType introduced in eaf6dbf5

modules/core/contentTypes/contentTypes.module.js
@@ -15,8 +15,7 @@ install: install,
     author: 'cliftonc',
     version: '0.1.1',
     home:'http://github.com/cliftonc/calipso'
-  },
-  depends:["content"]
+  }
 };
 
 /**
@@ -251,7 +250,6 @@ function editContentTypeForm(req,res,template,block,next) {
  */
 function updateContentType(req,res,template,block,next) {
 
-
   calipso.form.process(req,function(form) {
 
     if(form) {
@@ -260,7 +258,7 @@ function updateContentType(req,res,template,block,next) {
         var id = req.moduleParams.id;
 
         ContentType.findById(id, function(err, c) {
-          if (c) {
+          if (!err && c) {
 
               c.contentType = form.contentType.contentType;
               c.description = form.contentType.description;