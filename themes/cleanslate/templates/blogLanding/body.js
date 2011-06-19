7733f0dbfefc281c01413accd5edf5b13d483e28
cliftonc <clifton.cunningham@gmail.com>
2011-06-19 13:25:31 +0100

    Fixed bugs in landing pages in clean slate - inside the js

--- themes/cleanslate/templates/blogLanding/body.js
+++ themes/cleanslate/templates/blogLanding/body.js
@@ -16,9 +16,9 @@ exports = module.exports = function(req,options,callback) {
       // But you can override on an individual query by setting in the options (second param)
       var query;
       if(options.user.isAdmin) {
-        query = new Query({'meta.contentType':'Blog'});
+        query = new Query({'contentType':'Blog'});
       } else {
-        query = new Query({'meta.contentType':'Blog', 'status':'published'});
+        query = new Query({'contentType':'Blog', 'status':'published'});
       }
       options.getContentList(query,{req:req,pager:false},this.parallel());
 
