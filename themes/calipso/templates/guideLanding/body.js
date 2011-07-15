7a9a89b65eba70b49878ec927c30a04a885b4599
cliftonc <clifton.cunningham@gmail.com>
2011-07-15 07:39:24 +0100

    Updated landing pages in calipso theme

---
+++ themes/calipso/templates/guideLanding/body.js
@@ -0,0 +1,35 @@
+/**
+ * Additional content section / block functions for body.
+ */
+
+var calipso = require("lib/calipso"), Query = require("mongoose").Query;
+
+exports = module.exports = function(req,options,callback) {
+
+  /**
+   *  Get additional content for blocks in the template
+   */
+  calipso.lib.step(
+    function getContentList() {
+
+      // Create a query and retrieve the content, has pager support, using req.moduleParams
+      // But you can override on an individual query by setting in the options (second param)
+      var query;
+      query = new Query();
+      query.where('taxonomy',/^guide.*/);
+      
+      if(!options.user.isAdmin) {
+        query.where('status','published');
+      }
+      
+      options.getContentList(query,{req:req,sortBy:'published,desc',pager:false},this.parallel());
+
+    },
+    function done(err, output) {
+
+      callback(err,{ contents:output.contents, pager:output.pager });
+
+    }
+  );
+
+}
