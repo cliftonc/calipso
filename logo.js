31606f7cfdc4fed209f96ffc91033ea30d0b316b
cliftonc <clifton.cunningham@gmail.com>
2011-08-05 09:11:17 +0100

    Externalised logo

---
+++ logo.js
@@ -0,0 +1,23 @@
+/*!
+ * Calipso Ascii Logo
+ *
+ * Copyright(c) 2011 Clifton Cunningham <clifton.cunningham@gmail.com>
+ * MIT Licensed
+ *
+ */
+
+require('colors');
+
+exports.print = function() {
+
+  console.log("");
+  console.log("            _ _                    ".yellow.bold);
+  console.log("  ___  __ _| (_)_ __  ___  ___     ".yellow.bold);
+  console.log(" / __|/ _` | | | '_ \\/ __|/ _ \\  ".green.bold);
+  console.log("| (__| (_| | | | |_) \\__ \\ (_) | ".blue.bold);
+  console.log(" \\___|\\__,_|_|_| .__/|___/\\___/ ".magenta.bold);
+  console.log("               |_|                 ".magenta.bold);
+  console.log("");
+
+ }
+
