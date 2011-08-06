4c898dba1365fe07776202437553e52c0657890f
Grey <conancat@gmail.com>
2011-08-07 00:59:32 +0800

    jade version of cleanslate theme

---
+++ themes/cleanslate-jade/templates/home/body.js
@@ -0,0 +1,34 @@
+/**
+ * Additional content section / block functions for body.
+ */
+
+var calipso = require("../../../../lib/calipso");
+
+exports = module.exports = function(req, options, callback) {
+
+  /**
+   *  Get additional content for blocks in the template
+   */
+  calipso.lib.step(
+
+  function getContent() {
+    options.getContent(req, "welcome-text", this.parallel());
+    options.getContent(req, "home-about-calipso", this.parallel());
+    options.getContent(req, "home-quickstart", this.parallel());
+    options.getContent(req, "home-guide", this.parallel());
+    options.getContent(req, "home-feature-a", this.parallel());
+    options.getContent(req, "home-feature-b", this.parallel());
+    options.getContent(req, "home-feature-c", this.parallel());
+  }, function done(err, welcome, about, quickstart, guide, fa, fb, fc) {
+    callback(err,{
+      welcome: welcome,
+      about: about,
+      quickstart: quickstart,
+      guide: guide,
+      featurea: fa,
+      featureb: fb,
+      featurec: fc
+    });
+  });
+
+}
