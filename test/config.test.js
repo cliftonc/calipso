b963b978c18d1cbd7f278fbc7c26d8f49edc3e11
cliftonc <clifton.cunningham@gmail.com>
2011-08-12 17:33:29 +0100

    Added new config library based on nconf, need to put in place automated transisiton from old to new - still using old for now

---
+++ test/config.test.js
@@ -0,0 +1,92 @@
+/**
+ *  Sanity test
+ *  Test must be executed serially as it runs
+ *  against an app instance, this is typically executed by the make file
+ **/
+
+var rootpath = process.cwd() + '/',
+    fs = require('fs'),
+    exec = require('child_process').exec,
+    path = require('path'),
+    assert = require('assert'),
+    sys = require('sys'),
+    should = require('should'),
+    Config = require(rootpath + 'lib/Config').Config;
+
+var defaultConfig = {
+  "test":"test"
+};
+
+/**
+ * Tests
+ */
+exports['I can create a development configuration'] = function() {
+
+  mkdir(path.join(rootpath,'tmp'), function() {
+    fs.writeFileSync(path.join(rootpath,'tmp','default.json'),JSON.stringify(defaultConfig));
+    var conf = new Config({env:'development',path:path.join(rootpath,'tmp')});
+    conf.type.should.equal('file');
+    conf.file.should.equal(path.join(rootpath,'tmp','development.json'));
+  });
+
+};
+
+exports['I can add and retrieve configuration'] = function() {
+
+    var conf = new Config({env:'development',path:path.join(rootpath,'tmp')});
+    conf.set('test:v1','v1');
+    conf.get('test:v1').should.equal('v1');
+    conf.set('test:v2','v2');
+    conf.get('test:v2').should.equal('v2');
+
+    var test = conf.get('test');
+    test.v1.should.equal('v1');
+    test.v2.should.equal('v2');
+
+};
+
+exports['I can use different environments'] = function() {
+
+    var confDev = new Config({env:'development',path:path.join(rootpath,'tmp')});
+    var confTest = new Config({env:'test',path:path.join(rootpath,'tmp')});
+
+    confDev.set('test:v1','v1');
+    confDev.get('test:v1').should.equal('v1');
+    confDev.save(function(err) {
+        path.existsSync(confDev.file);
+    });
+
+    confTest.set('test:v1','v1');
+    confTest.get('test:v1').should.equal('v1');
+    confTest.save(function(err) {
+        path.existsSync(confTest.file);
+    });
+
+};
+
+exports['I can use the setSave shortcut'] = function() {
+
+    var conf = new Config({path:path.join(rootpath,'tmp')});
+    conf.setSave('test:setsave','Yah!',function(err) {
+        var confTest = new Config({path:path.join(rootpath,'tmp')});
+        confTest.get('test:setsave').should.equal('Yah!');
+    });
+
+};
+
+
+/**
+ * Mkdir -p.
+ *
+ * TODO - these are unix only ...
+ *
+ * @param {String} path
+ * @param {Function} fn
+ */
+
+function mkdir(path, fn) {
+  exec('mkdir -p ' + path, function(err){
+    if (err) throw err;
+    fn && fn();
+  });
+}
