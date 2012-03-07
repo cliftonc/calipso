43de866073b245f97e27393540b518205ccf8718
Andreas Richter <andy@richteralsi.com>
2012-03-06 22:46:58 -0500

    Beginnings of supporting 0.6.x cluster.

--- app-cluster.js
+++ app-cluster.js
@@ -7,34 +7,50 @@ var port = process.env.PORT || 3000;
 var path = __dirname;
 var app;
 
-/**
- * Create an instance of calipso via the normal App,
- */
-require('./app').boot(function (app) {
-
+if (!cluster.use) {
+  if (cluster.isMaster) {
+    var cpuCount = require('os').cpus().length;
+    while (cpuCount-- > 0) {
+      cluster.fork();
+    }
+    cluster.on('death', function(worker) {
+        console.log('worker ' + worker.pid + ' died');
+    });
+  } else {
+    console.log("worker " + process.pid);
+    require('./app').boot(function (app) {
+      app.listen(port);
+    }, true);
+  }
+} else {
   /**
-   * TODO: Check to ensure that the logs and pids folders exist before launching
+   * Create an instance of calipso via the normal App,
    */
-console.log(cluster);
-  cluster(app)
-    .set('working directory', path)
-    .set('socket path', path)
-    .in('development')
-    .set('workers', 3)
-    .use(cluster.logger(path + '/logs', 'debug'))
-    .use(cluster.debug())
-    .use(cluster.pidfiles(path + '/pids'))
-    .use(cluster.stats({ connections: true, lightRequests: true }))
-    .in('test')
-    .set('workers', 3)
-    .use(cluster.logger(path + '/logs', 'warning'))
-    .use(cluster.pidfiles(path + '/pids'))
-    .in('production')
-    .set('workers', 3)
-    .use(cluster.logger(path + '/logs'))
-    .use(cluster.pidfiles(path + '/pids'))
-    .in('all')
-    .listen(port);
+  require('./app').boot(function (app) {
+
+    /**
+     * TODO: Check to ensure that the logs and pids folders exist before launching
+     */
+    cluster(app)
+      .set('working directory', path)
+      .set('socket path', path)
+      .in('development')
+      .set('workers', 3)
+      .use(cluster.logger(path + '/logs', 'debug'))
+      .use(cluster.debug())
+      .use(cluster.pidfiles(path + '/pids'))
+      .use(cluster.stats({ connections: true, lightRequests: true }))
+      .in('test')
+      .set('workers', 3)
+      .use(cluster.logger(path + '/logs', 'warning'))
+      .use(cluster.pidfiles(path + '/pids'))
+      .in('production')
+      .set('workers', 3)
+      .use(cluster.logger(path + '/logs'))
+      .use(cluster.pidfiles(path + '/pids'))
+      .in('all')
+      .listen(port);
 
 
-},true);
+  },true);
+}
--- app.js
+++ app.js
@@ -130,7 +130,6 @@ exports.boot = function (next,cluster) {
   var Config = require(path + "/lib/Config").Config;
   app.config = new Config();
   app.config.init();
-  console.log("App Config is " + app.config);
 
     // Load application configuration
     // theme = app.config.get('themes:front');
