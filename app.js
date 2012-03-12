273c340d002083c240372f93a1e56501c0a30d2d
Andreas Richter <andy@richteralsi.com>
2012-03-12 17:59:24 -0400

    Switch to latest express and connect

--- app.js
+++ app.js
@@ -148,7 +148,8 @@ if (!module.parent) {
       app.listen(port);
       console.log("Calipso version: ".green + app.about.version);
       console.log("Calipso configured for: ".green + (global.process.env.NODE_ENV || 'development') + " environment.".green);
-      console.log("Calipso server listening on port: ".green + app.address().port);
+      if (app.address)
+        console.log("Calipso server listening on port: ".green + app.address().port);
     } else {
       console.log("\r\nCalipso terminated ...\r\n".grey);
       process.exit();
