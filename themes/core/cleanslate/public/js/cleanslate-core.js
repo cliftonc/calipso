380fd1d8517585de7f474014e5562552800688de
Sean Goresht <seangores@gmail.com>
2013-01-02 21:57:39 -0700

    Add Aloha editor
    Add aloha editor for editing your content

--- themes/core/cleanslate/public/js/cleanslate-core.js
+++ themes/core/cleanslate/public/js/cleanslate-core.js
@@ -22,81 +22,101 @@ cleanslate = {
 	backlog: [],
 	setResponsiveMenu: function ()
 	{
-			if (Modernizr.mq("media handheld, only screen and (max-width: 767px)")) {
-				console.log("Doing menu resize...");
-				var $menus = $("ul.menu");
-				var $menuItems = $menus.find("li");
-				$menuItems.toggle();
-				var $responsiveMenuItemTitle = "<li class='menu-item'><a href='#' class='showMenuItems'><i class='icon-list-2 responsive-logo'></i></a> </li> ";
+		if (Modernizr.mq('only screen and (max-width: 767px)') || Modernizr.mq('handheld'))
+		{
+			console.log("Doing menu resize...");
+			var $menus = $("ul.menu");
+			var $menuItems = $menus.find("li");
+			$menuItems.toggle();
+			var $responsiveMenuItemTitle = "<li class='menu-item'><a href='#' class='showMenuItems'><i class='icon-list-2 responsive-logo'></i></a></li> ";
 
-				$menus.append($responsiveMenuItemTitle).on('click', function (e)
-				{
-					e.preventDefault();
-					$menuItems.toggle("slow");
-					$(this).find(".responsive-logo").toggleClass("icon-minus-2");
-				});
-			}
+/*			$menus.append($responsiveMenuItemTitle).on('click', function (e)
+			{
+				e.preventDefault();
+				$menuItems.toggle("slow");
+				$(this).find(".responsive-logo").toggleClass("icon-minus-2");
+			});	*/
+
+			$menus.append($responsiveMenuItemTitle);
+			var $responsiveLogo = $(".responsive-logo");
+			$responsiveLogo.parent().parent().on('click', function (e)
+			{
+				e.preventDefault();
+				$menuItems.toggle("slow");
+				$responsiveLogo.toggleClass("icon-minus-2");
+			});
+
+		}
+	}, checkJson: function ()
+	{
+		hasJson = false;
+		if (!Modernizr.json)
+		{
+			console.log("this browser doesn't support local storage.")
+			$.getScript("/js/store.min.js");
+		}
 	}, init: function ()
 	{
 
 		// HIDE/SHOW USER LOGIN FORM
+		this.checkJson();
 		(function doUsername()
 		{
-			var userDataString = $.cookie('userData') || localStorage.user || '{}';
-			var user = JSON.parse(decodeURIComponent(userDataString == 'undefined' ? '{}' : userDataString));
-			var userWelcomeOrLoginBox = $('#user-welcome-or-login');
+				var userDataString = $.cookie('userData') || localStorage.user || '{}';
+				var user = JSON.parse(decodeURIComponent(userDataString == 'undefined' ? '{}' : userDataString));
+				var userWelcomeOrLoginBox = $('#user-welcome-or-login');
 
-			console.log(user);
+				console.log(user);
 
-			if (user.username)
-			{
-				// logged in
-				userWelcomeOrLoginBox.html(
-					'<a href="/user">' + user.username + '</a> | ' +
-						// we could get fancy and check whether the first letter of the username is uppercase
-						// and if so, make "Log Out" uppercase to match.
-						'<a href="/user/logout?returnto=' + location.href + '">log out</a>'
-				);
-
-				//if($.cookie('pendingUserAction')){
-				//
-				//}
-
-			}
-			else
-			{
-				// not logged in
-				userWelcomeOrLoginBox.find('a').eq(0).click(function (e)
+				if (user.username)
 				{
-					// if the 'plain layout' module is active, then prevent default and get the login form via ajax
-					e.preventDefault();
-					var el = $(this);
-					var userLoginBox = $('#user-login');
-					if (!userLoginBox.length)
+					// logged in
+					userWelcomeOrLoginBox.html(
+						'<a href="/user">' + user.username + '</a> | ' +
+							// we could get fancy and check whether the first letter of the username is uppercase
+							// and if so, make "Log Out" uppercase to match.
+							'<a href="/user/logout?returnto=' + location.href + '">log out</a>'
+					);
+
+					//if($.cookie('pendingUserAction')){
+					//
+					//}
+
+				}
+				else
+				{
+					// not logged in
+					userWelcomeOrLoginBox.find('a').eq(0).click(function (e)
 					{
-						$.get(el.attr('href'), function (text)
+						// if the 'plain layout' module is active, then prevent default and get the login form via ajax
+						e.preventDefault();
+						var el = $(this);
+						var userLoginBox = $('#user-login');
+						if (!userLoginBox.length)
 						{
-							var start = '<!--PAGE_BODY_START-->';
-							var end = '<!--PAGE_BODY_END-->';
-							if (text.length && text.indexOf(start) > -1 && text.indexOf(end) > -1)
+							$.get(el.attr('href'), function (text)
 							{
-								var html = '<div class="close"></div>' + text.split(start)[1].split(end)[0];
-								userLoginBox = $('<div id="user-login" class="threecol"/>').html(html).appendTo(userWelcomeOrLoginBox).show();
-								userLoginBox.find('input')[0].focus();
-								userWelcomeOrLoginBox.find('.close').click(function ()
+								var start = '<!--PAGE_BODY_START-->';
+								var end = '<!--PAGE_BODY_END-->';
+								if (text.length && text.indexOf(start) > -1 && text.indexOf(end) > -1)
 								{
-									userLoginBox.toggle();
-								});
-							}
-						});
-					}
-					else
-					{
-						userLoginBox.toggle();
-						userLoginBox.find('input')[0].focus();
-					}
-				});
-			}
+									var html = '<div class="close"></div>' + text.split(start)[1].split(end)[0];
+									userLoginBox = $('<div id="user-login" class="threecol"/>').html(html).appendTo(userWelcomeOrLoginBox).show();
+									userLoginBox.find('input')[0].focus();
+									userWelcomeOrLoginBox.find('.close').click(function ()
+									{
+										userLoginBox.toggle();
+									});
+								}
+							});
+						}
+						else
+						{
+							userLoginBox.toggle();
+							userLoginBox.find('input')[0].focus();
+						}
+					});
+				}
 		})();
 
 		// FLASH MESSAGING
@@ -235,33 +255,11 @@ cleanslate = {
 
 		// REMOVE ANNOYING TITLE TOOLTIPS FROM MENUS
 		/*$('.menu a').each(function (i, node)
-		{
-			node.title = "";
-		});*/
+		 {
+		 node.title = "";
+		 });*/
 
 		this.setResponsiveMenu();
-		/*$(".menu li li").toggle("slow").parent().on('hover', function (e)
-		{
-			$(this).find("li").toggle("slow");
-		});*/
-
-		if (Modernizr.mq('only screen and (max-width: 767px)')) {
-			console.log("Doing menu resize...");
-			var $menus = $("ul.menu");
-			var $menuItems = $menus.find("li");
-			$menuItems.toggle();
-			var $responsiveMenuItemTitle = "<li class='menu-item'><a href='#' class='showMenuItems'><i class='icon-list-2 responsive-logo'></i></a> </li> ";
-
-			$menus.append($responsiveMenuItemTitle).on('click', function (e)
-			{
-				e.preventDefault();
-				$menuItems.toggle("slow");
-				$(this).find(".responsive-logo").toggleClass("icon-minus-2");
-			});
-		}
-
-		
-
 	} // end of cleanslate.init
 
 }; //end of cleanslate
