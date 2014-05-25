59ed2c0584eeaf783b992cb2e9e44c01a598512b
Andreas Richter <andy@richteralsi.com>
2014-05-25 10:27:16 -0400

    mongodb needs to be installed as part of calipso and not the final site.

--- bin/siteInstall.sh
+++ bin/siteInstall.sh
@@ -2,5 +2,5 @@
 # Simple bash installation script for Calipso
 # We have to manually install mongodb with native (this does mean its done twice)
 echo "Installing mongodb in native mode ..."
-npmResult=$(npm install mongodb --mongodb:native)
+npmResult=$(npm install --mongodb:native)
 exit 0
